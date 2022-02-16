ce94600825343b95b14eb81253631fcef20a3bed
Ivan Jankov <ivanjankov07@gmail.com>
2022-02-16 19:32:17 +0100

    Slide start and get position functions create

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const slider = document.getElementById('slider'),
 	slides = Array.from(document.querySelectorAll('.single_slide')),
 	sliderImages = Array.from(document.querySelectorAll('.slider_image'));
 
+let startPos = 0,
+	isDragging = false,
+	currTranslate = 0,
+	curPosition = 0,
+	prevTranslate = 0;
+
 // prevent default behaviour when dragging images
+
 imgPrevDefault();
 
 function imgPrevDefault() {
@@ -18,7 +25,19 @@ function imgPrevDefault() {
 slides.forEach((slide) => {
 	// pc listeners
 	slide.addEventListener('mousedown', slideStart());
-	slide.addEventListener('mouseleave', slideEnd);
-	slide.addEventListener('mousemove', slideMove);
-	slide.addEventListener('mouseup', slideEnd);
+	// slide.addEventListener('mouseleave', slideEnd);
+	// slide.addEventListener('mousemove', slideMove);
+	// slide.addEventListener('mouseup', slideEnd);
 });
+
+function slideStart() {
+	return function (event) {
+		curPosition = getPosition(event);
+		isDragging = true;
+		console.log(curPosition);
+	};
+}
+
+function getPosition(event) {
+	return event.pageX;
+}
